bae0e5e8b3e4338d0e8ec253ddcf690240bad19a
PatrickYeh <vetom198@gmail.com>
2016-04-08 15:46:01 +0800

    setup postgres connection info

--- src/config.js
+++ src/config.js
@@ -10,16 +10,12 @@
 /* eslint-disable max-len */
 /* jscs:disable maximumLineLength */
 
-export const port = process.env.PORT || 3000;
-export const host = process.env.WEBSITE_HOSTNAME || `localhost:${port}`;
-
-export const databaseUrl = process.env.DATABASE_URL || 'postgresql://demo:Lqk62xg6TBm5UhfR@demo.ctbl5itzitm4.us-east-1.rds.amazonaws.com:5432/membership01';
-
+export const port = process.env.PORT || 80;
+export const host = `p-da.club:${port}`;
+export const databaseUrl = process.env.DATABASE_URL || 'postgresql://postgres:a5mkdDJCDYRO4n-RpKN3mjbGYpWTi4UZ@192.168.0.95:5432/cycling';
 export const analytics = {
-
   // https://analytics.google.com/
   google: { trackingId: process.env.GOOGLE_TRACKING_ID || 'UA-XXXXX-X' },
-
 };
 
 export const auth = {
@@ -28,8 +24,8 @@ export const auth = {
 
   // https://developers.facebook.com/
   facebook: {
-    id: process.env.FACEBOOK_APP_ID || '186244551745631',
-    secret: process.env.FACEBOOK_APP_SECRET || 'a970ae3240ab4b9b8aae0f9f0661c6fc',
+    id: process.env.FACEBOOK_APP_ID || '826176954154496',
+    secret: process.env.FACEBOOK_APP_SECRET || '77c35aa72ddf05f848c12b8bc4ca397e',
   },
 
   // https://cloud.google.com/console/project
@@ -43,5 +39,4 @@ export const auth = {
     key: process.env.TWITTER_CONSUMER_KEY || 'Ie20AZvLJI2lQD5Dsgxgjauns',
     secret: process.env.TWITTER_CONSUMER_SECRET || 'KTZ6cxoKnEakQCeSpZlaUCJWGAlTEBJj0y2EMkUBujA7zWSvaQ',
   },
-
 };
